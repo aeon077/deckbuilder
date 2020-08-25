9a3025180c18f10d8162608c8133dfacd2faa3c6
aeon077 <carolyn.schiltz@gmail.com>
2020-08-24 21:30:40 -0400

    updating day 3

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,57 @@
 import React from 'react';
 
+import './Card.css';
+
 const Card = ({
+    id,
     name,
     type,
     manaCost,
     text,
     flavor,
-    imageUrl
+    imageUrl,
+    addCardToDeck,
+    removeCardFromDeck
 }) => {
-    console.log(Card)
     return (
         <div className="Card">
-            {name} - { manaCost} <br />
-            {type} <br />
-            {text} <br />
-            {flavor} <br />
-            <img src={imageUrl} alt="" />
+            <div className="info">
+                <p className="header">
+                    {name} - {manaCost}
+                </p>
+                <p className="type">
+                    {type}
+                </p>
+                <p className="text">
+                    {text}
+                </p>
+                <p className="flavor">
+                    {flavor}
+                </p>
+            </div>
+            <img className="preview" src={imageUrl} alt="" />
+            <div className="actions">
+                <button
+                    onClick={() => {
+                        addCardToDeck({
+                            id,
+                            name
+                        })
+                    }}>
+                    (+) Add to Deck
+          </button>
+                <button
+                    onClick={() => {
+                        removeCardFromDeck({
+                            id
+                        })
+                    }}>
+                    (-) Remove from Deck
+        </button>
+            </div>
         </div>
     );
 }
 
 export default Card;
+
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import { fetchCards } from '../api';
+import React, { useState } from 'react';
+
+import {
+    fetchCards,
+} from '../api';
 
 const SearchBar = ({ setResults }) => {
-    console.log("Rendering Search")
+    const [name, setName] = useState(''); //name/text sets value to onSubmit in return element
+    const [text, setText] = useState('');
+
+    const handleNameChange = event => {
+        setName(event.target.value);
+    }
+
+    const handleTextChange = event => {
+        setText(event.target.value);
+    }
+
     async function handleSubmit(event) {
         event.preventDefault();
-        const cards = await fetchCards();
+
+        const cards = await fetchCards({
+            name,
+            text
+        });
+
         setResults(cards);
     }
+
     return (
         <div id="search">
             <h3>Look up cards here...</h3>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="card search" />
+                <input
+                    type="text"
+                    placeholder="card name"
+                    value={name}
+                    onChange={handleNameChange} />
+                <input
+                    type="text"
+                    placeholder="card text"
+                    value={text}
+                    onChange={handleTextChange} />
                 <button type="submit">Search</button>
             </form>
         </div>
-    )
-};
+    );
+}
 
 export default SearchBar;
